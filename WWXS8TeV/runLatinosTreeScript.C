aae7f391f141120aa74ae37b8532f29c5a7cf118
piedra <>
2012-08-22 16:41:31 +0000

    Minor cosmetics

--- WWXS8TeV/runLatinosTreeScript.C
+++ WWXS8TeV/runLatinosTreeScript.C
@@ -2,7 +2,7 @@ void runLatinosTreeScript(Double_t luminosity    = 5.064,
 			  Int_t    jetChannel    = 0,
 			  TString  flavorChannel = "SF",
 			  TString  theSample     = "TW",
-			  Bool_t   verbose       = false)
+			  Bool_t   verbose       = true)
 {
   gInterpreter->LoadMacro("LatinosTreeScript.C+");
 
