e004a96519161b36cbe7f679c12bb4bf2eb07951
piedra <>
2012-09-12 17:00:20 +0000

    Added standAlone DY and Top macros

---
+++ WWXS8TeV/standAloneDY.C
@@ -0,0 +1,35 @@
+#include "DY.C"
+
+
+TString directory = "rootfiles_highmass/rootfiles_noCorrection";
+//TString directory = "rootfiles_highmass/rootfiles_type1";
+//TString directory = "rootfiles_highmass/rootfiles_type1_xy";
+//TString directory = "rootfiles_highmass/rootfiles_type1_xy_smear";
+
+
+//------------------------------------------------------------------------------
+//
+// standAloneDY
+//
+//------------------------------------------------------------------------------
+void standAloneDY(Int_t  njet          = 0,
+		  Bool_t useDataDriven = true,
+		  Int_t  printLevel    = 2,
+		  Bool_t drawR         = true)
+{
+  Double_t dyYield;
+  Double_t dyStatError;
+  Double_t dySystError;
+  Double_t dyScaleFactor;
+
+  DY(dyYield,
+     dyStatError,
+     dySystError,
+     dyScaleFactor,
+     njet,
+     "SF",
+     directory,
+     useDataDriven,
+     printLevel,
+     drawR);
+}
---
+++ WWXS8TeV/standAloneTop.C
@@ -0,0 +1,50 @@
+#include "DY.C"
+#include "Top.C"
+
+
+TString directory = "rootfiles_noCorrection";
+//TString directory = "rootfiles_type1";
+//TString directory = "rootfiles_type1_xy";
+//TString directory = "rootfiles_type1_xy_smear";
+
+
+//------------------------------------------------------------------------------
+//
+// standAloneTop
+//
+//------------------------------------------------------------------------------
+void standAloneTop(Int_t  njet          = 0,
+		   Bool_t useDataDriven = true,
+		   Int_t  printLevel    = 2)
+{
+  Double_t dyYield;
+  Double_t dyStatError;
+  Double_t dySystError;
+  Double_t dyScaleFactor;
+
+  DY(dyYield,
+     dyStatError,
+     dySystError,
+     dyScaleFactor,
+     njet,
+     "SF",
+     directory,
+     useDataDriven,
+     printLevel);
+
+  Double_t topYield;
+  Double_t topStatError;
+  Double_t topSystError;
+  Double_t topScaleFactor;
+
+  Top(topYield,
+      topStatError,
+      topSystError,
+      topScaleFactor,
+      dyScaleFactor,
+      njet,
+      "All",
+      directory,
+      useDataDriven,
+      printLevel);
+}
