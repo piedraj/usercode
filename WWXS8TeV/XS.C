c2035f03f661d449e3a027e06bd457beb9b34270
piedra <>
2013-04-18 17:12:17 +0000

    Recovered AGAIN Zgamma

--- WWXS8TeV/XS.C
+++ WWXS8TeV/XS.C
@@ -23,12 +23,13 @@ const UInt_t Nsyst = 11;
                                 
 // systematic[i] = 1e2 * systematic / N
 //------------------------------------------------------------------------------
-TString  Systematics    [Nsyst] = {"trigger", "lepton", "#mu scale",  "e scale",  "MET", "jet veto", "PU", "Wg", "PDF", "corrections", "sample"};
-Double_t SystematicsggWW[Nsyst] = {      1.5,      2.0,         1.5,        2.5,    2.0,        4.7,  2.3,  0.0,   0.8,          30.0,      3.1};
-Double_t SystematicsqqWW[Nsyst] = {      1.5,      2.0,         1.5,        2.5,    2.0,        4.7,  2.3,  0.0,   2.3,           1.5,      1.1};
-Double_t SystematicsVV  [Nsyst] = {      1.5,      2.0,         1.5,        1.9,    2.0,        4.7,  2.3,  0.0,   5.9,           3.3,      4.1};
-Double_t SystematicsWg  [Nsyst] = {      1.5,      2.0,         1.5,        2.0,    2.0,        4.7,  2.3, 30.0,   0.0,           0.0,      8.4};
-Double_t SystematicsH125[Nsyst] = {      1.5,      2.0,         1.5,        2.5,    2.0,        4.7,  2.3,  0.0,   2.3,           1.5,      1.1}; // [*]
+TString  Systematics      [Nsyst] = {"trigger", "lepton", "#mu scale",  "e scale",  "MET", "jet veto", "PU", "Wg", "PDF", "corrections", "sample"};
+Double_t SystematicsggWW  [Nsyst] = {      1.5,      2.0,         1.5,        2.5,    2.0,        4.7,  2.3,  0.0,   0.8,          30.0,      3.1};
+Double_t SystematicsqqWW  [Nsyst] = {      1.5,      2.0,         1.5,        2.5,    2.0,        4.7,  2.3,  0.0,   2.3,           1.5,      1.1};
+Double_t SystematicsVV    [Nsyst] = {      1.5,      2.0,         1.5,        1.9,    2.0,        4.7,  2.3,  0.0,   5.9,           3.3,      4.1};
+Double_t SystematicsWg    [Nsyst] = {      1.5,      2.0,         1.5,        2.0,    2.0,        4.7,  2.3, 30.0,   0.0,           0.0,      8.4};
+Double_t SystematicsH125  [Nsyst] = {      1.5,      2.0,         1.5,        2.5,    2.0,        4.7,  2.3,  0.0,   2.3,           1.5,      1.1}; // [*]
+Double_t SystematicsZgamma[Nsyst] = {      1.5,      2.0,         1.5,        2.5,    2.0,        4.7,  2.3,  0.0,   2.3,           1.5,      1.1}; // [*]
 
 
 // [*] Using SystematicsqqWW as placeholder
@@ -127,7 +128,8 @@ void XS(Double_t &xsValue,
   TFile* inputDY       = new TFile(path + "DY.root");  
   TFile* inputDYtautau = new TFile(path + "DYtautau.root");  
   TFile* inputWg       = new TFile(path + "Wgamma.root");  
-  TFile* inputH125     = new TFile(path + "HWW125.root");  
+  TFile* inputH125     = new TFile(path + "HWW125.root"); 
+  TFile* inputZgamma   = new TFile(path + "Zgamma.root");  
   TFile* inputData     = new TFile(path + "DataRun2012_Total.root");
 
   TFile* inputDYOF = new TFile(Form("%s/%djet/OF/DY.root", directory.Data(), njet));
@@ -150,6 +152,7 @@ void XS(Double_t &xsValue,
   TH1F* hNDYtautau = (TH1F*) inputDYtautau->Get("hW" + analysisLevel);
   TH1F* hNWg       = (TH1F*) inputWg      ->Get("hW" + analysisLevel);
   TH1F* hNH125     = (TH1F*) inputH125    ->Get("hW" + analysisLevel);
+  TH1F* hNZgamma   = (TH1F*) inputZgamma  ->Get("hW" + analysisLevel);
   TH1F* hNData     = (TH1F*) inputData    ->Get("hW" + analysisLevel);
   TH1F* hNDYOF     = (TH1F*) inputDYOF    ->Get("hW" + analysisLevel);
 
@@ -165,6 +168,7 @@ void XS(Double_t &xsValue,
   Double_t NDYtautau[] = {hNDYtautau->GetBinContent(2), hNDYtautau->GetBinError(2), 0.50*hNDYtautau->GetBinContent(2)}; // [*]
   Double_t NWg[]       = {hNWg      ->GetBinContent(2), hNWg      ->GetBinError(2), 0.00};
   Double_t NH125[]     = {hNH125    ->GetBinContent(2), hNH125    ->GetBinError(2), 0.00};
+  Double_t NZgamma[]   = {hNZgamma  ->GetBinContent(2), hNZgamma  ->GetBinError(2), 0.00};
   Double_t NData[]     = {hNData    ->GetBinContent(2), hNData    ->GetBinError(2)};
 
 
@@ -209,12 +213,14 @@ void XS(Double_t &xsValue,
   // Add relative systematic uncertainties
   //----------------------------------------------------------------------------
   for (UInt_t iSyst=0; iSyst<Nsyst; iSyst++) {
-    NggWW[2] += (SystematicsggWW[iSyst] * SystematicsggWW[iSyst]);
-    NqqWW[2] += (SystematicsqqWW[iSyst] * SystematicsqqWW[iSyst]);
-    NWZ  [2] += (SystematicsVV  [iSyst] * SystematicsVV  [iSyst]);
-    NZZ  [2] += (SystematicsVV  [iSyst] * SystematicsVV  [iSyst]);
-    NWg  [2] += (SystematicsWg  [iSyst] * SystematicsWg  [iSyst]);
-    NH125[2] += (SystematicsH125[iSyst] * SystematicsH125[iSyst]);
+    NggWW  [2] += (SystematicsggWW  [iSyst] * SystematicsggWW  [iSyst]);
+    NqqWW  [2] += (SystematicsqqWW  [iSyst] * SystematicsqqWW  [iSyst]);
+    NWZ    [2] += (SystematicsVV    [iSyst] * SystematicsVV    [iSyst]);
+    NZZ    [2] += (SystematicsVV    [iSyst] * SystematicsVV    [iSyst]);
+    NWg    [2] += (SystematicsWg    [iSyst] * SystematicsWg    [iSyst]);
+    NH125  [2] += (SystematicsH125  [iSyst] * SystematicsH125  [iSyst]);
+    NZgamma[2] += (SystematicsZgamma[iSyst] * SystematicsZgamma[iSyst]);
+
   }
 
   NggWW[2] = sqrt(NggWW[2]);  // Relative systematic uncertainty
@@ -224,12 +230,13 @@ void XS(Double_t &xsValue,
   NWW[2] += (NqqWW[2]*NqqWW[0] * NqqWW[2]*NqqWW[0]);
   NWW[2]  = sqrt(NWW[2]) / NWW[0];  // Relative systematic uncertainty
 
-  NWZ  [2] = sqrt(NWZ[2])   * NWZ[0]   / 1e2;  // Absolute systematic uncertainty
-  NZZ  [2] = sqrt(NZZ[2])   * NZZ[0]   / 1e2;  // Absolute systematic uncertainty
-  NWg  [2] = sqrt(NWg[2])   * NWg[0]   / 1e2;  // Absolute systematic uncertainty
-  NH125[2] = sqrt(NH125[2]) * NH125[0] / 1e2;  // Absolute systematic uncertainty
+  NWZ    [2] = sqrt(NWZ    [2]) * NWZ    [0] / 1e2;  // Absolute systematic uncertainty
+  NZZ    [2] = sqrt(NZZ    [2]) * NZZ    [0] / 1e2;  // Absolute systematic uncertainty
+  NWg    [2] = sqrt(NWg    [2]) * NWg    [0] / 1e2;  // Absolute systematic uncertainty
+  NH125  [2] = sqrt(NH125  [2]) * NH125  [0] / 1e2;  // Absolute systematic uncertainty
+  NZgamma[2] = sqrt(NZgamma[2]) * NZgamma[0] / 1e2;  // Absolute systematic uncertainty
 
-  Double_t Background = NWj[0] + NWZ[0] + NZZ[0] + NWg[0] + NTop[0] + NDY[0] + NDYtautau[0] + NH125[0];
+  Double_t Background = NWj[0] + NWZ[0] + NZZ[0] + NWg[0] + NTop[0] + NDY[0] + NDYtautau[0] + NH125[0] + NZgamma[0];
   
   Double_t statErrorB = sqrt(NWj      [1]*NWj      [1] +
 			     NWZ      [1]*NWZ      [1] +
@@ -238,7 +245,8 @@ void XS(Double_t &xsValue,
 			     NTop     [1]*NTop     [1] +
 			     NDY      [1]*NDY      [1] +
 			     NDYtautau[1]*NDYtautau[1] +
-			     NH125    [1]*NH125    [1]);
+			     NH125    [1]*NH125    [1] +
+			     NZgamma  [1]*NZgamma  [1]);
 
   Double_t systErrorB = sqrt(NWj      [2]*NWj      [2] +
 			     NWZ      [2]*NWZ      [2] +
@@ -247,7 +255,8 @@ void XS(Double_t &xsValue,
 			     NTop     [2]*NTop     [2] +
 			     NDY      [2]*NDY      [2] +
 			     NDYtautau[2]*NDYtautau[2] +
-			     NH125    [2]*NH125    [2]);
+			     NH125    [2]*NH125    [2] +
+			     NZgamma  [2]*NZgamma  [2]);
 
   Double_t totalErrorB = sqrt(statErrorB*statErrorB + systErrorB*systErrorB);
 
@@ -351,6 +360,7 @@ void XS(Double_t &xsValue,
     printf("       Z/g*                & %6.1f $\\pm$ %5.1f $\\pm$ %5.1f",   NDY[0]+NDYtautau[0], statErrorDYAll,  systErrorDYAll);
     printf(" (%.1f Z/g* + %.1f Z/g* -> tautau)\n",                           NDY[0],              NDYtautau[0]);
     printf("       Wg+Wg*              & %6.1f $\\pm$ %5.1f $\\pm$ %5.1f\n", NWg[0],              NWg[1],          NWg[2]);
+    printf("       Zgamma              & %6.1f $\\pm$ %5.1f $\\pm$ %5.1f\n", NZgamma[0],          NZgamma[1],      NZgamma[2]);
     printf("       H125                & %6.1f $\\pm$ %5.1f $\\pm$ %5.1f\n", NH125[0],            NH125[1],        NH125[2]);
     printf("       total background    & %6.1f $\\pm$ %5.1f $\\pm$ %5.1f\n", Background,          statErrorB,      systErrorB);
     printf("       signal              & %6.1f $\\pm$ %5.1f $\\pm$ %5.1f\n", NWW[0],              NWW[1],          NWW[2]*NWW[0]/1e2);
