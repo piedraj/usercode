966fa1fdcd3e9e6aac56d7e021988fe3128f9140
piedra <>
2013-08-14 10:44:08 +0000

    Updated submit macros

--- WZ/submitEverythingForMe.sh
+++ WZ/submitEverythingForMe.sh
@@ -6,13 +6,13 @@ rm -rf results
 
 root -l -b -q loadAnalysisWZ.C
 
-./Submit.sh 0 0 -1 20 35
-./Submit.sh 0 0  0 20 35
-./Submit.sh 0 0  1 20 35
+./submit.sh 0 0 -1 20 35
+./submit.sh 0 0  0 20 35
+./submit.sh 0 0  1 20 35
 
-./Submit.sh 8 1 -1 20 35
-./Submit.sh 8 1  0 20 35
-./Submit.sh 8 1  1 20 35
+./submit.sh 8 1 -1 20 35
+./submit.sh 8 1  0 20 35
+./submit.sh 8 1  1 20 35
 
 rm -rf job_*.sh
 
