b65db8da3a51c25ef2ecc5ec3985d8f83318de3a
piedraj <jonatan.piedra@gmail.com>
2013-11-07 12:12:57 +0100

    Test commit

--- WZ/runXS.sh
+++ WZ/runXS.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 
-rm -rf pdf
-rm -rf png
+rm -rf *d
+rm -rf *log
+rm -rf *so
+rm -rf *~
+rm -rf *_C.*
+rm -rf *_C_*
+rm -rf  job_XS_*.sh
+rm -rf  pdf
+rm -rf  png
+rm -rf  tex
 
 
 root -l -b -q loadXS.C
 
 
-for ((cut=0; cut<10; cut++))
+for ((cut=0; cut<15; cut++))
   do
   for ((mode=0; mode<2; mode++))
     do
@@ -16,7 +24,6 @@ for ((cut=0; cut<10; cut++))
       do
       for ((njet=-1; njet<2; njet++))
 	do
-#	root -l -b -q "XS.C+($cut,$mode,$wcharge,$njet)"
 	./submitXS.sh $cut $mode $wcharge $njet
       done
     done
@@ -24,10 +31,7 @@ for ((cut=0; cut<10; cut++))
 done
 
 
-# Cleanup
-#-------------------------------------------------------------------------------
-rm -rf *d
-rm -rf *log
-rm -rf *so
-rm -rf *~
+root -l -b -q drawScaleFactors.C+
+root -l -b -q drawWeights.C+
+root -l -b -q drawElectronScaleSystematic.C+
 
