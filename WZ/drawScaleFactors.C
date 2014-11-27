6c52fe8e4cc33c4d2b18c1700ef437defdff8017
piedraj <jonatan.piedra@gmail.com>
2014-11-27 19:35:32 +0100

    Added FR histograms

--- WZ/drawScaleFactors.C
+++ WZ/drawScaleFactors.C
@@ -11,35 +11,19 @@ const Float_t _bigLabelSize = 0.04;
 
 // Member functions
 //------------------------------------------------------------------------------
-void      DrawIt             (TH2F*   h,
-			      TString title);
+void DrawIt             (TString filename,
+			 TString hname,
+			 TString cname,
+			 TString title);
 
-void      AxisFonts          (TAxis*  axis,
-			      TString title);
+void AxisFonts          (TAxis*  axis,
+			 TString title);
 
-void      TH2FAxisFonts      (TH2F*   h,
-			      TString coordinate,
-			      TString title);
+void TH2FAxisFonts      (TH2F*   h,
+			 TString coordinate,
+			 TString title);
 
-TH2F*     LoadHistogram      (TString filename,
-			      TString hname,
-			      TString cname);
-
-void      MakeOutputDirectory(TString format);
-
-
-// Data members
-//------------------------------------------------------------------------------
-TH2F*   MuonSF;
-TH2F*   ElecSF;
-TH2F*   MuonPR;
-TH2F*   ElecPR;
-TH2F*   MuonFR;
-TH2F*   ElecFR;
-TH2F*   DoubleElLead;
-TH2F*   DoubleMuLead;
-TH2F*   DoubleElTrail;
-TH2F*   DoubleMuTrail;
+void MakeOutputDirectory(TString format);
 
 
 void drawScaleFactors()
@@ -47,40 +31,43 @@ void drawScaleFactors()
   MakeOutputDirectory("pdf");
   MakeOutputDirectory("png");
 
+  DrawIt("MuSF_2012",  "h2inverted", "MuonSF", "muon SF (8 TeV)");
+  DrawIt("EleSF_2012", "h2inverted", "ElecSF", "electron SF (8 TeV)");
 
-  // SF, FR and PR histograms
-  //----------------------------------------------------------------------------
-  MuonSF = LoadHistogram("MuSF_2012",            "h2inverted", "MuonSF");
-  ElecSF = LoadHistogram("EleSF_2012",           "h2inverted", "ElecSF");
-  MuonPR = LoadHistogram("MuPR_Moriond13_2012",  "h2inverted", "MuonPR");
-  ElecPR = LoadHistogram("ElePR_Moriond13_2012", "h2inverted", "ElecPR");
+  DrawIt("MuPR_Moriond13_2012",  "h2inverted", "MuonPR", "muon PR (8 TeV)");
+  DrawIt("ElePR_Moriond13_2012", "h2inverted", "ElecPR", "electron PR (8 TeV)");
 
-  MuonFR = LoadHistogram("MuFR_Moriond13_jet20_EWKcorr",  "FR_pT_eta_EWKcorr", "MuonFR_Jet20");
-  ElecFR = LoadHistogram("EleFR_Moriond13_jet35_EWKcorr", "fakeElH2",          "ElecFR_Jet35");
-
-  DoubleElLead  = LoadHistogram("triggerEfficiencies", "DoubleElLead",  "DoubleElLead");
-  DoubleMuLead  = LoadHistogram("triggerEfficiencies", "DoubleMuLead",  "DoubleMuLead");
-  DoubleElTrail = LoadHistogram("triggerEfficiencies", "DoubleElTrail", "DoubleElTrail");
-  DoubleMuTrail = LoadHistogram("triggerEfficiencies", "DoubleMuTrail", "DoubleMuTrail");
-
-  DrawIt(MuonSF,        "#sqrt{s} = 8 TeV muon SF");
-  DrawIt(ElecSF,        "#sqrt{s} = 8 TeV electron SF");
-  DrawIt(MuonPR,        "#sqrt{s} = 8 TeV muon PR");
-  DrawIt(ElecPR,        "#sqrt{s} = 8 TeV electron PR");
-  DrawIt(MuonFR,        "#sqrt{s} = 8 TeV muon FR jet20");
-  DrawIt(ElecFR,        "#sqrt{s} = 8 TeV electron FR jet35");
-  DrawIt(DoubleElLead,  "#sqrt{s} = 8 TeV leading electron trigger efficiency");
-  DrawIt(DoubleMuLead,  "#sqrt{s} = 8 TeV leading muon trigger efficiency");
-  DrawIt(DoubleElTrail, "#sqrt{s} = 8 TeV trailing electron trigger efficiency");
-  DrawIt(DoubleMuTrail, "#sqrt{s} = 8 TeV trailing muon trigger efficiency");
+  DrawIt("MuFR_Moriond13_jet10_EWKcorr",  "FR_pT_eta_EWKcorr", "MuonFR_Jet10", "muon FR jet10 (8 TeV)");
+  DrawIt("MuFR_Moriond13_jet20_EWKcorr",  "FR_pT_eta_EWKcorr", "MuonFR_Jet20", "muon FR jet20 (8 TeV)");
+  DrawIt("MuFR_Moriond13_jet30_EWKcorr",  "FR_pT_eta_EWKcorr", "MuonFR_Jet30", "muon FR jet30 (8 TeV)");
+  
+  DrawIt("EleFR_Moriond13_jet15_EWKcorr", "fakeElH2", "ElecFR_Jet15", "electron FR jet15 (8 TeV)");
+  DrawIt("EleFR_Moriond13_jet35_EWKcorr", "fakeElH2", "ElecFR_Jet35", "electron FR jet35 (8 TeV)");
+  DrawIt("EleFR_Moriond13_jet50_EWKcorr", "fakeElH2", "ElecFR_Jet50", "electron FR jet50 (8 TeV)");
+  
+  DrawIt("triggerEfficiencies", "DoubleElLead",  "DoubleElLead",  "leading electron trigger efficiency (8 TeV)");
+  DrawIt("triggerEfficiencies", "DoubleMuLead",  "DoubleMuLead",  "leading muon trigger efficiency (8 TeV)");
+  DrawIt("triggerEfficiencies", "DoubleElTrail", "DoubleElTrail", "trailing electron trigger efficiency (8 TeV)");
+  DrawIt("triggerEfficiencies", "DoubleMuTrail", "DoubleMuTrail", "trailing muon trigger efficiency (8 TeV)");
 }
 
 
 //------------------------------------------------------------------------------
 // DrawIt
 //------------------------------------------------------------------------------
-void DrawIt(TH2F* h, TString title)
+void DrawIt(TString filename,
+	    TString hname,
+	    TString cname,
+	    TString title)
 {
+  TFile* inputfile = TFile::Open("../AuxiliaryFilesWZXS8TeV/" + filename + ".root");
+
+  TH2F* h = (TH2F*)inputfile->Get(hname)->Clone(cname);
+
+  h->SetDirectory(0);
+
+  inputfile->Close();
+
   TString name = h->GetName();
 
   TCanvas* canvas = new TCanvas(name, name, 600, 600);
@@ -183,25 +170,6 @@ void TH2FAxisFonts(TH2F*   h,
   if (coordinate.Contains("y")) axis = h->GetYaxis();
 
   AxisFonts(axis, title);
-}
-
-
-//------------------------------------------------------------------------------
-// LoadHistogram
-//------------------------------------------------------------------------------
-TH2F* LoadHistogram(TString filename,
-		    TString hname,
-		    TString cname)
-{
-  TFile* inputfile = TFile::Open("../AuxiliaryFilesWZXS8TeV/" + filename + ".root");
-
-  TH2F* hist = (TH2F*)inputfile->Get(hname)->Clone(cname);
-
-  hist->SetDirectory(0);
-
-  inputfile->Close();
-
-  return hist;
 }
 
 
